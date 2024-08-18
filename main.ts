29b21d7ba850ac00bdddce26004e9e91ba1cbd83
aryu-ki <aryuki9701@gmail.com>
2024-08-18 15:11:01 +0300

    feat: Added debug settings

--- main.ts
+++ main.ts
@@ -3,10 +3,12 @@ import { App, EventRef, MarkdownRenderChild, Plugin, PluginSettingTab, Setting, 
 
 interface MyPluginSettings {
 	mySetting: string;
+	debug: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'default',
+	debug: false
 }
 
 export default class MyPlugin extends Plugin {
@@ -14,12 +16,14 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
+		console.log(`Debug mode ${this.settings.debug ? 'ON' : 'OFF'}`)
 		this.registerMarkdownCodeBlockProcessor("evals", (source, element, context) => {
-			const scoreRenderChild = new ScoreRenderChild(element, source, this.app.workspace);
+			const scoreRenderChild = new ScoreRenderChild(element, source, this.app.workspace, this.settings.debug);
 			context.addChild(scoreRenderChild);
 			scoreRenderChild.load()
 		})
+
+		this.addSettingTab(new DebugSettingTab(this.app, this))
 	}
 
 	onunload() {
@@ -31,6 +35,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async saveSettings() {
+		console.log(`Debug mode ${this.settings.debug ? 'ON' : 'OFF'}`)
 		await this.saveData(this.settings);
 	}
 }
@@ -39,10 +44,12 @@ class ScoreRenderChild extends MarkdownRenderChild {
 	private source: string
 	private workspace: Workspace
 	private eventRef: EventRef
-	constructor(container: HTMLElement, source: string, workspace: Workspace) {
+	private debug: boolean
+	constructor(container: HTMLElement, source: string, workspace: Workspace, debug?: boolean) {
 		super(container)
 		this.source = source
 		this.workspace = workspace
+		this.debug = debug == null ? false : debug
 	}
 
 	onload(): void {
@@ -57,24 +64,54 @@ class ScoreRenderChild extends MarkdownRenderChild {
 	private async render() {
 		const editor = this.workspace.activeEditor?.editor
 		if (!editor) {
+			if (this.debug) console.log(`[DEBUG] No active editor`)
 			return
 		}
 		const lineCount = editor.lineCount()
 			let score = 0
+			if (this.debug) console.log(`[DEBUG] Starting processing`)
 			for (let i = 0; i < lineCount; i++) {
 				const line = editor.getLine(i)
 				if (line.startsWith("- [x")) {
-					console.log(`Found line containing required score payload: ${line}`)
+					if (this.debug) console.log(`[DEBUG] Found line containing required score payload: ${line}`)
 					const scorePayload = /\[\s*-?(\d+)\s*\]/.exec(line)?.[1]
 					if (scorePayload != null) {
-						console.log(`[DEBUG] Matched score payload: ${scorePayload}`)
-						console.log(`[DEBUG] parseInt(scorePayload.trim()): ${parseInt(scorePayload.trim())}`)
+						if (this.debug) console.log(`[DEBUG] Matched score payload: ${scorePayload}`)
+						if (this.debug) console.log(`[DEBUG] parseInt(scorePayload.trim()): ${parseInt(scorePayload.trim())}`)
 						score += parseInt(scorePayload.trim())
 					}
 				}
 			}
+			if (this.debug) console.log(`[DEBUG] Processing complete. Final score is ${score}`)
         const content = this.containerEl.createEl('h1');
 		content.textContent = this.source.replace("!score!", `${score}`);
         this.containerEl.firstChild?.replaceWith(content);
     }
+}
+
+class DebugSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+	}
+
+	display(): void {
+    let { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+		.setName("Debug")
+		.setDesc("Enable console logs for debugging")
+		.addToggle((toggle) => {
+			toggle.setValue(this.plugin.settings.debug)
+			toggle.onChange(async (value) => {
+				this.plugin.settings.debug = value;
+				await this.plugin.saveSettings();
+			})
+		}
+		);
+	}
 }
